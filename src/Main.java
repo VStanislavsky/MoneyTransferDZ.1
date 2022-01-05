6bdeef2b5d857af3cf655fe762cd5c16af228e62
VStanislavsky <stanislavvlasov@mail.ru>
2022-01-05 08:35:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int balans = 2_000_000_000;
+        int refill = 500_000_000;
+        int totalSam = balans + refill;
+        System.out.println(totalSam);
+    }
+}
